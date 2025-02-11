6872c318d9945d840b91fc6e897e6fc9550f8004
Marin Mario <38869813+MarinMario@users.noreply.github.com>
2025-02-11 13:56:19 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <div style={{
+      backgroundColor: "black",
+      color: "white",
+      textAlign: "center",
+      borderRadius: "10px",
+      padding: "10px"
+    }}>
+      Hi
+    </div>
+  )
+}
+
+export default App
